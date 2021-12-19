dedb5ff4cdebca9c21f9d77535908aa0af70b793
TuanDao2002 <s3877347@rmit.edu.vn>
2021-12-19 20:51:16 +0700

    update request

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const url = '';
 
 const inputField = document.querySelector("input");
-const submit = document.querySelector("#submit");
-const responseField = document.querySelector("responseField");
+const buttons = document.querySelectorAll("button");
+const responseField = document.querySelector("#responseField");
 
-const sendRequest = () => {
+const sendRequest = (button) => {
     const location = inputField.value;
-    const command = submit.value;
-    const endpoint = url + location + '/' + command
+    const command = button.value;
+    const endpoint = url + location + '/' + command;
 
     const xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
@@ -20,9 +20,21 @@ const sendRequest = () => {
                 responseField.innerHTML = `<h2>Request sent</h2>`;
             } 
         } else {
+            // test
+            setTimeout(function(){
+                responseField.innerHTML = `<h2>${command}</h2>`;
+            }, 1000)   
+            
             responseField.innerHTML = `<h2>Sending request...</h2>`;
         }
     }
 
     xhr.send();
 }
+
+buttons.forEach(button => {
+    //in arrow function, this always refer to global variable
+    button.addEventListener('click', function(){
+        sendRequest(this);
+    });
+})
