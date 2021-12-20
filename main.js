423423525495975b9a0a62bac271bc56130d2950
TuanDao2002 <s3877347@rmit.edu.vn>
2021-12-20 21:07:26 +0700

    request api

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const url = '';
+const url = 'http://localhost:3000/';
 
 const inputField = document.querySelector("input");
 const buttons = document.querySelectorAll("button");
@@ -9,6 +9,11 @@ const sendRequest = (button) => {
     const command = button.value;
     const endpoint = url + location + '/' + command;
 
+    if (location === "") {
+        responseField.innerHTML = `<h2>You have not entered location</h2>`;
+        return;
+    }
+
     const xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
@@ -17,14 +22,19 @@ const sendRequest = (button) => {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             var status = xhr.status;
             if (status === 0 || (status >= 200 && status < 400)) {
-                responseField.innerHTML = `<h2>Request sent</h2>`;
+                const res = xhr.response;
+                let info;
+                let name;
+                if (command === "temp") {
+                    name = "Temperature";
+                    info = res.temperature + "&degC";
+                } else if (command === "humid") {
+                    name = "Humidity";
+                    info = res.humidity + "%";
+                }
+                responseField.innerHTML = `<h2>${name}: ${info} </h2>`;
             } 
-        } else {
-            // test
-            setTimeout(function(){
-                responseField.innerHTML = `<h2>${command}</h2>`;
-            }, 1000)   
-            
+        } else {        
             responseField.innerHTML = `<h2>Sending request...</h2>`;
         }
     }
